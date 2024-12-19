fdb3c1875055567e3c7d948932fb97850a2d2f57
aravindkk004 <aravind463kk30@gmail.com>
2024-12-19 21:15:39 +0530

    Updated

--- src/components/Gradient.jsx
+++ src/components/Gradient.jsx
@@ -3,14 +3,14 @@ const Gradient = () => {
     <div
       className="before:content-[''] after:content-[''] before:absolute after:absolute  
         before:rounded-full after:rounded-full 
-        md:before:w-[22.125rem] before:w-[10.125rem] 
-        md:after:w-[22.125rem] after:w-[15.125rem] 
-        after:h-[15.125rem] md:after:h-[22.125rem] 
-        before:h-[22.125rem] md:before:h-[10.125rem] 
+        md:before:w-[22.125rem] before:w-[17.125rem] 
+        md:after:w-[22.125rem] after:w-[17.125rem] 
+        after:h-[17.125rem] md:after:h-[22.125rem] 
+        before:h-[17.125rem] md:before:h-[22.125rem] 
         before:bg-[#7e61e7] after:bg-[#7e61e7] 
         before:blur-[225px] after:blur-[225px] 
         before:-top-12 before:-left-20 
-        after:-bottom-30 after:right-0 -z-10"
+        after:-bottom-25 after:right-0 -z-10"
     ></div>
   );
 };
